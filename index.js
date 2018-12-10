92cf3200f7b9cdb37ab0bfc273ddc657f7f8a7dd
Julien <julien.linder@gmail.com>
2018-12-11 00:58:54 +0100

    sans wireframe

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function init() {
 
   // on créé un cube au quel on définie un matériau puis on l'ajoute à la scène
   const geometry = new THREE.CubeGeometry(200, 200, 200);
-  const material = new THREE.MeshBasicMaterial({ color: Oxff0000, wireframe: true });
+  const material = new THREE.MeshBasicMaterial({ color: Oxff0000, wireframe: false });
   mesh = new THREE.Mesh(geometry, material);
   scene.add(mesh);
   // on ajoute une lumière blanche
