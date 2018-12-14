bd503ce2e5b05fceb223e384a432285316afa89f
Julien <julien.linder@gmail.com>
2018-12-14 23:59:13 +0100

    wip externalisation de fonction + add eslint config

--- index.js
+++ index.js
@@ -1,76 +1,98 @@
-let renderer, scene, camera, mesh;
+let renderer, scene, camera;
+const univers = [];
 
+const sphere = (radius, color) => {
+  const geometry = new THREE.SphereGeometry(radius, radius / 2, radius / 2);
+  const material = new THREE.MeshBasicMaterial({ color, wireframe: true });
+  return new THREE.Mesh(geometry, material);
+};
 
-function init() {
-  
+const makePivot = (pivotObject, planete) => {
+  console.log(pivotObject, planete);
+  // create pivot object
+  const pivot = new THREE.Object3D();
+  pivotObject.add(pivot);
+
+  // add planet to pivot
+  pivot.add(planete);
+  return pivot;
+};
+
+const createSollarSystem = (size) => {
+  const sollarSystem = new THREE.Object3D();
+  const sun = sphere(100, 0xf0f00f);
+  univers.push({ threeObj: sun, rotate: { x: 0, y: 0.007, z: 0 } });
+  sollarSystem.add(sun);
+  if (size === 1) {
+    return sollarSystem;
+  }
+
+  // on créé une sphere au quel on définie un matériau puis on l'ajoute à la scène
+  const planet = sphere(50, 0x00ffff);
+  univers.push(planet);
+  const pivotPoint = makePivot(sun, planet);
+  univers.push(pivotPoint);
+  // on donne une position à la seconde sphere
+  planet.position.set(260, 4, 6);
+  console.log(sun);
+  return sollarSystem;
+};
+
+const init = () => {
+
   // on initialise le moteur de rendu
   renderer = new THREE.WebGLRenderer();
-  
+
   // si WebGL ne fonctionne pas sur votre navigateur on peut utiliser le moteur de rendu Canvas à la place
   // renderer = new THREE.CanvasRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
   document.getElementById('container').appendChild(renderer.domElement);
-  
+
   // on initialise la scène
   scene = new THREE.Scene();
-  
+
   // on initialise la camera que l'on place ensuite sur la scène
   camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 1, 10000);
   camera.position.set(300, 400, 700);
   scene.add(camera);
-  
-  // on créé une sphere au quel on définie un matériau puis on l'ajoute à la scène
-  const SphereGeometry1 = new THREE.SphereGeometry(100, 50, 50);
-  const SphereMaterial1 = new THREE.MeshBasicMaterial({ color: 0xf0f00f, wireframe: true });
-  mesh1 = new THREE.Mesh(SphereGeometry1, SphereMaterial1);
-  scene.add(mesh1);
-  console.log(mesh1.position);
-  //on ajoute un point de pivot sur la première sphere
-  pivotPoint = new THREE.Object3D();
-  mesh1.add(pivotPoint);
 
-// on créé une sphere au quel on définie un matériau puis on l'ajoute à la scène
-  const SphereGeometry2 = new THREE.SphereGeometry (50, 20, 20);
-  const SphereMaterial2 = new THREE.MeshBasicMaterial({ color: 0xfeeee, wireframe: true});
-  mesh2 = new THREE.Mesh(SphereGeometry2, SphereMaterial2);
+  const sollarSystem = createSollarSystem(2);
+  scene.add(sollarSystem);
 
-  //on donne une position à la seconde sphere 
-  mesh2.position.set(260, 4, 6);
-
-  // on ajoute un point de pivot à la seconde sphere
-  pivotPoint.add(mesh2);
-  
   // on ajoute une lumière blanche
   const lumiere = new THREE.DirectionalLight(0xffffff, 1.0);
   lumiere.position.set(0, 0, 400);
   scene.add(lumiere);
-  
+
   // on effectue le rendu de la scène
   renderer.render(scene, camera);
-}
+};
 
-function animate() {
+const animate = () => {
   // on appel la fonction animate() récursivement à chaque frame
   requestAnimationFrame(animate);
-  
+
   // const SpherePosition = mesh1.position.set(100, 100, 100);
   // SpherePosition.position.set(10, 10, 10);
-  
+
   camera.lookAt(scene.position);
   // on fait tourner la sphere sur ses axes x et y
-  const time = Date.now()* 0.00;
-  mesh1.position.x = Math.cos(time*10)*5;
-  mesh1.position.y = Math.cos(time*7)*3;
-  mesh1.position.z = Math.cos(time*8)*4;
-  pivotPoint.rotation.y += 0.010;
-
-  mesh1.rotation.x += 0.00;
-  mesh1.rotation.y += 0.009;
-  mesh2.rotation.x += 0.00;
-  mesh2.rotation.y += 0.05;
+  // const time = Date.now() * 0.00;
+  // mesh2.position.x = Math.sin(time * 10) * 5;
+  // mesh2.position.y = Math.sin(time * 7) * 3;
+  // mesh2.position.z = Math.sin(time * 8) * 4;
+  const { threeObj: { rotation }, rotate: { x: rx, y: ry, z: rz } } = univers[0];
+  const { x, y, z } = rotation;
+  rotation.set(x + rx, y + ry, z + rz);
+  // pivotPoint.rotation.y += 0.09;
+  // mesh1.rotation.x += 0.00;
+  // mesh1.rotation.y += 0.00;
+  // mesh1.position.x += 0.05;
+  // mesh2.rotation.x += 0.00;
+  // mesh2.rotation.y += 0.00;
   // on effectue le rendu de la scène
   renderer.render(scene, camera);
-}
+};
 
 init();
 animate();
