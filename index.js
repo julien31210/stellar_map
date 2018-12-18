6ed5a944daaebfe88733fbf38f298737fa18f4e7
Julien <julien.linder@gmail.com>
2018-12-18 23:04:23 +0100

    function range externalize

--- index.js
+++ index.js
@@ -3,21 +3,6 @@ const univers = [];
 const speed = 3; // 3 px per frame
 const mouseSen = 2;
 
-const range = (start, end, scl) => {
-  const emp = (end - start);
-  const r = [];
-  if (emp > 0) {
-    for (let i = start; i <= end; i += emp / scl) { r.push(i); }
-  } else {
-    console.log({ empscl: emp / scl, start, end, scl });
-    for (let i = end; i <= start; i += -(emp / scl)) {
-      console.log(emp - i);
-      r.push(emp - i);
-    }
-  }
-  return r;
-};
-
 const keys = {};
 let mousepressed = false;
 
