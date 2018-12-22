42565be992f044d64b5b08c079ccdb4da92d727a
Cyril <cyril-denoyelle@hotmail.fr>
2018-12-22 19:01:03 +0100

    add conf system

---
+++ public/js/config.example.js
@@ -0,0 +1,14 @@
+const global = {
+    controls: {
+        forward: '90',
+        back: '83',
+        right: '68',
+        left: '81',
+        up: '32',
+        down: '17',
+        roll: {
+            right:'69',
+            left:'65',
+        }
+    }
+}
