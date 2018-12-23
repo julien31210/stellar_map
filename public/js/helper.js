0c1f1c76235b694d775f9a706daf4b719d539d5c
Cyril <cyril-denoyelle@hotmail.fr>
2018-12-23 01:43:11 +0100

    remove logs in range function

--- public/js/helper.js
+++ public/js/helper.js
@@ -4,9 +4,7 @@ const range = (start, end, scl) => {
   if (emp > 0) {
     for (let i = start; i <= end; i += emp / scl) { r.push(i); }
   } else {
-    console.log({ empscl: emp / scl, start, end, scl });
     for (let i = end; i <= start; i += -(emp / scl)) {
-      console.log(emp - i);
       r.push(emp - i);
     }
   }
