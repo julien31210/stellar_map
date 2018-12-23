f486fe81b38ce51107eaa84efa000141b759ca18
Cyril <cyril-denoyelle@hotmail.fr>
2018-12-23 01:51:12 +0100

    add logger in conf example

--- public/js/config.example.js
+++ public/js/config.example.js
@@ -1,14 +1,15 @@
 const global = {
-    controls: {
-        forward: '90',
-        back: '83',
-        right: '68',
-        left: '81',
-        up: '32',
-        down: '17',
-        roll: {
-            right:'69',
-            left:'65',
-        }
-    }
+  controls: {
+    forward: '90',
+    back: '83',
+    right: '68',
+    left: '81',
+    up: '32',
+    down: '17',
+    roll: {
+      right: '69',
+      left: '65',
+    },
+    logger: '76'
+  }
 }
