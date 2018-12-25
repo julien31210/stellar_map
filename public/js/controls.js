b3c2a942ae2e13f2b562a61280d7812d375ebd77
cyrild <cyril-denoyelle@hotmail.fr>
2018-12-25 18:05:56 +0100

    remove config and add controls

---
+++ public/js/controls.js
@@ -0,0 +1,36 @@
+const controls = {
+  qwerty: {
+    forward: '90',
+    back: '83',
+    right: '68',
+    left: '81',
+    up: '32',
+    down: '17',
+    roll: {
+      right: '69',
+      left: '65',
+    },
+    logger: '76',
+    timeSpeed: {
+      speedUp: '82',
+      slowDown: '70'
+    }
+  },
+  azerty: {
+    forward: '87',
+    back: '83',
+    right: '68',
+    left: '65',
+    up: '32',
+    down: '16',
+    roll: {
+      right: '69',
+      left: '81',
+    },
+    logger: '76',
+    timeSpeed: {
+      speedUp: '82',
+      slowDown: '70'
+    }
+  }
+};
