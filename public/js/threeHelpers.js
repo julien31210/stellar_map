04e1966468abce5c8b0e032e9d35bf7689b94049
cyrild <cyril-denoyelle@hotmail.fr>
2018-12-29 16:43:11 +0100

    normalise size and distances + fix rightclic on the sun

--- public/js/threeHelpers.js
+++ public/js/threeHelpers.js
@@ -23,12 +23,12 @@ const sunlight = (color, intensity, scope, radius) => {
   const sollar = sphere(radius, color, 'basic');
   const light = new THREE.PointLight(color, intensity, scope);
 
-  return light.add(sollar);
+  return sollar.add(light);
 };
 
-const createSollarSystem = (size) => { // eslint-disable-line
+const createSollarSystem = () => {
   const sun = new Astre({
-    radius: (69.57 * (10 ** 5)) / dimentionsDivider,
+    radius: 6.957 * (10 ** 6) / dimentionsDivider,
     color: 0xf0f00f,
     mass: (1.989 * (10 ** 30)) / dimentionsDivider,
     type: 'star'
@@ -36,24 +36,26 @@ const createSollarSystem = (size) => { // eslint-disable-line
   univers.push(sun);
 
   const earth = new Astre({
-    radius: 6371 / dimentionsDivider,
+    radius: 6.371 * (10 ** 3) / dimentionsDivider,
     color: 0x00ffff,
     type: 'planet',
     orbit: {
       parent: sun,
-      distance: (149 * (10 ** 6)) / dimentionsDivider,
+      distance: 1.49 * (10 ** 8) / dimentionsDivider,
+      tilt: 30,
     },
-    mass: (5.972 * (10 ** 24)) / dimentionsDivider
+    mass: 5.972 * (10 ** 24) / dimentionsDivider
   });
   univers.push(earth);
 
   const moon = new Astre({
-    radius: 1737 / dimentionsDivider,
+    radius: 1.737 * (10 ** 3) / dimentionsDivider,
     color: 0xcccccc,
     type: 'planet',
     orbit: {
       parent: earth,
-      distance: (384 * (10 ** 3)) / dimentionsDivider,
+      distance: 3.84 * (10 ** 5) / dimentionsDivider,
+      eccentricity: 10,
     },
     mass: (7.36 * (10 ** 22)) / dimentionsDivider
   });
