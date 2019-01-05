ea22a6acced564866204cf88d22d5fbeb1bab02c
cyrild <cyril-denoyelle@hotmail.fr>
2019-01-05 20:50:34 +0100

    tilt to rad in initThreeObj

--- public/js/class/Astre.js
+++ public/js/class/Astre.js
@@ -61,7 +61,7 @@ class Astre {
     // calculate Orbit things
     this.orbit.eccentricity = aprox(eccentricity, aproxValues && aproxValues.eccentricity) || 0;
     this.orbit.distance = aprox(distance, aproxValues && aproxValues.distance) || 0;
-    // this.orbit.tilt = aprox(tilt, aproxValues && aproxValues.tilt) || 0;
+    this.orbit.tilt = convert.radians(aprox(tilt, aproxValues && aproxValues.tilt) || 0);
 
     this.uuid = this.threeObj.uuid;
     scene.add(this.threeObj);
@@ -82,8 +82,8 @@ class Astre {
       if (this.radialPosition > PI * 2) this.radialPosition -= PI * 2;
 
       this.threeObj.position.x = parent.threeObj.position.x + distance * eccentricity + cos(this.radialPosition) * (distance + distance * abs(eccentricity));
-      this.threeObj.position.z = parent.threeObj.position.z + sin(this.radialPosition) * (distance - (distance * sin(convert.radians(tilt))));
-      this.threeObj.position.y = parent.threeObj.position.y + sin(this.radialPosition) * distance * sin(convert.radians(tilt));
+      this.threeObj.position.z = parent.threeObj.position.z + sin(this.radialPosition) * (distance - (distance * sin(tilt)));
+      this.threeObj.position.y = parent.threeObj.position.y + sin(this.radialPosition) * distance * sin(tilt);
     }
     if (this.nestedAnimate && typeof this.nestedAnimate === 'function') this.nestedAnimate(delta);
   }
