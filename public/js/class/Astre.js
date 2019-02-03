4ff794a1ac91d049d8835ffe90554b500abafe52
cyrild <cyril-denoyelle@hotmail.fr>
2019-02-03 18:53:33 +0100

    rework animate function + large works on three ojects

--- public/js/class/Astre.js
+++ public/js/class/Astre.js
@@ -1,166 +1,218 @@
 
 class Astre {
   constructor(args) {
-    // { radius, color, type, mass, orbit }
-    // DOC
-    // orbit = {
-    //   parent = orbital direct parent,
-    //   distance = distance between parent and this astral object,
-    // }
+    this.baseThreeObj = new THREE.Object3D();
+    this.uuid = this.baseThreeObj.uuid;
+    this.lifeTime = 0;
+    this.realLifeTime = 0;
+    this.called = [];
 
     Object.keys(args)
       .forEach((key) => {
         this[key] = args[key];
       });
+
+    if (
+      this.density
+      && this.radius
+      && !this.mass
+    ) this.mass = this.density * (4 / 3 * Math.PI * (this.radius ** 3));
+
     this.childs = [];
     this.radialPosition = this.radialPosition || Math.PI;
     this.on = false;
+    this.threeObjInited = false;
     this.delayedDelta = 0;
+    this.childsIds = [];
 
-    if (typeof this.preconstruct === 'function') this.preconstruct();
+    this.position = {
+      x: (this.position && this.position.x) || 0,
+      y: (this.position && this.position.y) || 0,
+      z: (this.position && this.position.z) || 0
+    };
 
-    this.init();
+    if (this.orbit && this.orbit.parent) this.orbitAround();
+
+    scene.add(this.baseThreeObj);
+
+    cameraIndex.push(this);
+
+
   }
 
-  orbitAround(stellarParent) {
+  animate(delta, mult) {
+    this.lifeTime = delta * mult;
+    this.realLifeTime += delta;
+
+    if (camera && this.realLifeTime > 4) this.manageVisibility(camera);
+
+    if (this.realLifeTime > 2) {
+      // if (this.realDelayedDelta > 1) {
+      //   const delayed = this.delayedDelta;
+      //   this.delayedDelta = 0;
+
+      //   this.realDelayedDelta = 0;
+
+      //   const d = delta * mult + delayed;
+      //   this.move(d);
+
+      // } else if (this.on) {
+
+      const d = delta * mult;
+      this.move(d);
+
+      // } else {
+      //   this.delayedDelta += delta * mult;
+      //   this.realDelayedDelta += delta;
+      //   this.move(0);
+      // }
+    }
+
+    this.childs.forEach((el) => {
+      el.animate(delta, mult);
+    });
+
+
+  }
+
+  getDistanceToCamera(camera) {
+    const camVect = new THREE.Vector3();
+    camera.getWorldPosition(camVect);
+
+    return camVect.distanceTo(this.getWorldPosition());
+  }
+
+  getWorldPosition() {
+    const objVect = new THREE.Vector3();
+    this.baseThreeObj.getWorldPosition(objVect);
+
+    return objVect;
+  }
+
+  manageVisibility(camera) {
+    const d = this.getDistanceToCamera(camera);
+
+    if (this.manageLight && this.threeObjInited) this.manageLight(d);
+
+    if (d > 6000 * this.radius && this.on && this.threeObjInited) {
+      this.on = false;
+      scene.remove(this.baseThreeObj);
+    }
+
+    if (d < 6000 * this.radius && !this.on) {
+
+      if (!this.threeObjInited) {
+
+        this.initThreeObj();
+        this.threeObjInited = true;
+
+        setTimeout(() => {
+          if (this.getDistanceToCamera(camera) > 6000 * this.radius) {
+            console.log(`wrongInitThreeobj on ${this.type || this.constructor.name}${this.uuid.slice(0, 4)}`);
+          }
+        }, 100);
+      }
+      this.on = true;
+
+      scene.add(this.baseThreeObj);
+    }
+
+  }
+
+  move(delta, rad) {
+    if (this.orbit && this.orbit.parent) {
+      const { medianRadiantSpeed, radialPosition, orbit: { parent, distance: d, eccentricity: ecc, tilt } } = this;
+
+      const parentPos = parent.getWorldPosition();
+
+      const { PI } = Math;
+
+
+      if (d > 0) {
+        const { cos, sin, abs } = Math;
+
+        const radialStep = rad || medianRadiantSpeed * delta;
+
+        if (this.orbit.miror) {
+          this.radialPosition = this.orbit.miror.radialPosition + PI;
+        }
+        if (radialStep !== 0) this.radialPosition += radialStep;
+
+        if (this.radialPosition > PI * 2) this.radialPosition -= PI * 2;
+
+        const cosrad = cos(radialPosition);
+        const sinrad = sin(radialPosition);
+
+        this.position.x = parentPos.x
+          + d * (ecc || 0)
+          + cosrad * (d + d * abs(ecc || 0));
+
+        this.position.z = parentPos.z
+          + sinrad * d * cos(tilt);
+
+        this.position.y = parentPos.y
+          + sinrad * d * sin(tilt);
+
+      } else {
+        this.position.x = parentPos.x;
+        this.position.y = parentPos.y;
+        this.position.z = parentPos.z;
+      }
+
+    }
+
+    this.baseThreeObj.position.x = this.position.x;
+    this.baseThreeObj.position.y = this.position.y;
+    this.baseThreeObj.position.z = this.position.z;
+  }
+
+
+  orbitAround(stellarParent, center) {
     const { sqrt, PI } = Math;
     if (stellarParent) this.orbit.parent = stellarParent;
 
     const f = this.orbit.parent.childs.filter(el => el.uuid === this.uuid);
     if (!f.length) this.orbit.parent.childs.push(this);
-    if (this.orbit.parent) {
-      const { eccentricity, distance, tilt, aprox: aproxValues } = this.orbit;
 
-      // calculate Orbit things
-      this.orbit.eccentricity = aprox(eccentricity, aproxValues && aproxValues.eccentricity) || 0;
-      this.orbit.distance = aprox(distance, aproxValues && aproxValues.distance) || 0;
-      // this.orbit.tilt = convert.radians(aprox(tilt, aproxValues && aproxValues.tilt) || 0);
+    const mass = (center && center.mass)
+      ? center.mass
+      : this.orbit.parent && this.orbit.parent.mass;
+
+    const distance = (center && center.dist1 && center.dist2)
+      ? (center.dist1 ** 2) + center.dist2
+      : (this.orbit.distance ** 2) || 0;
+
+    if (mass) {
+      const { tilt } = this.orbit;
+
+      // tilt to radiants
       if (!this.orbit.tiltIsRadiants) {
         this.orbit.tilt = convert.radians(tilt);
         this.orbit.tiltIsRadiants = true;
       }
 
-      // console.log(this.name, tilt, convert.radians(tilt));
-
-      // console.log(this.name, 'orbitAround', this.orbit.parent.name, 'with mass', this.orbit.parent.mass, 'at', convert.to(this.orbit.distance));
-
       // speed of orbit in m/s to km/s
-      const orbitSpeed = sqrt(6.67 * (10 ** -11) * (this.orbit.parent.mass / dimentionDivider) / this.orbit.distance) / 1000;
+      const orbitSpeed = sqrt(
+        6.673 * (10 ** -11) // Gravitational force const
+        * mass
+        / distance
+      ) / 1000;
       // orbital period in s
       const orbitPeriod = PI * 2 * this.orbit.distance / orbitSpeed;
 
-      this.nominalRadiantSpeed = PI / orbitPeriod;
+      this.medianRadiantSpeed = (center && center.medianRadiantSpeed) || PI / orbitPeriod;
+
+      return { orbitPeriod, medianRadiantSpeed: this.medianRadiantSpeed };
     }
+
+    setTimeout(() => {
+      this.orbitAround(this.orbit.parent, center);
+    }, 1000);
+
   }
 
-  init() {
-
-    cameraIndex.push(this);
-
-    this.initThreeObj();
-
-    if (this.orbit && this.orbit.parent) {
-      this.orbitAround(this.orbit.parent);
-    }
-    // this.logMySelf();
-  }
-
-  logMySelf() {
-    console.log(this);
-  }
-
-  initThreeObj() {
-    const { radius, color } = this;
-
-    // make a sphere and put it in threeObj
-    const geometry = new THREE.SphereGeometry(radius, 25, 25);
-    const material = new THREE.MeshPhongMaterial({ color });
-    this.threeObj = new THREE.Mesh(geometry, material);
-
-    this.uuid = this.threeObj.uuid;
-  }
-
-  setBaseRadialPosition(rad) {
+  setRadialPosition(rad) {
     this.radialPosition = rad;
+
   }
-
-  astreAnimate(delta) {
-
-    // console.log(this.name, 'astreAnimate');
-    if (this.orbit && this.orbit.parent) {
-
-      const { nominalRadiantSpeed, radialPosition, orbit: { parent, distance: d, eccentricity: ecc, tilt } } = this;
-      const { cos, sin, PI, abs } = Math;
-
-
-      const radialStep = nominalRadiantSpeed * delta;
-
-      if (radialStep !== 0) this.radialPosition += radialStep;
-      if (this.radialPosition > PI * 2) this.radialPosition -= PI * 2;
-
-      const cosrad = cos(radialPosition);
-      const sinrad = sin(radialPosition);
-
-      this.threeObj.position.x = parent.threeObj.position.x
-        + d * ecc
-        + cosrad * (d + d * abs(ecc));
-
-      this.threeObj.position.z = parent.threeObj.position.z
-        + sinrad * d * cos(tilt);
-
-      this.threeObj.position.y = parent.threeObj.position.y
-        + sinrad * d * sin(tilt);
-    }
-  }
-
-  animate(delta) {
-
-    if (camera) this.manageVisibility(camera);
-
-    if (this.on) {
-      const delayed = this.delayedDelta;
-      this.delayedDelta = 0;
-
-      const d = delta + delayed;
-      this.astreAnimate(d);
-
-      if (this.childs && this.childs.length) {
-        this.childs.forEach((el) => {
-          el.astreAnimate(d);
-        });
-      }
-
-    } else {
-      this.delayedDelta += delta;
-    }
-
-    if (this.childs && this.childs.length) {
-      this.childs.forEach((el) => {
-        el.animate(delta);
-      });
-    }
-  }
-
-  manageVisibility(camera) {
-
-    const { position: p } = this.threeObj;
-    const { position: cp } = camera;
-    const objVect = new THREE.Vector3(p.x, p.y, p.z);
-    const camVect = new THREE.Vector3(cp.x, cp.y, cp.z);
-
-    this.threeObj.getWorldPosition(objVect);
-    camera.getWorldPosition(camVect);
-    const d = camVect.distanceTo(objVect);
-
-    if (d > 6000 * this.radius && this.on) {
-      this.on = false;
-      scene.remove(this.threeObj);
-    } else if (d < 6000 * this.radius && !this.on) {
-      this.on = true;
-      scene.add(this.threeObj);
-    }
-
-    if (this.manageLight) this.manageLight(d);
-  }
-
 }
